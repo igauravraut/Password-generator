7077d74193bb757eac796186bedaf930102ea007
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-02-04 04:12:22 +0530

    changes

---
+++ app.js
@@ -0,0 +1,31 @@
+const btn1 = document.querySelector(".btn");
+const input1 = document.getElementById("input");
+const copyIcon1 = document.querySelector(".fa-copy");
+
+
+btn1.addEventListener("click",()=>{
+    createPassword();
+})
+
+copyIcon1.addEventListener("click",()=>{
+    copyPassword();
+})
+
+function createPassword(){
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:<>?/~ ";
+
+    const passLength = 14;
+    let password = "";
+
+    for(let index = 0; index < passLength; index++){
+        const randomNum = Math.floor(Math.random() * chars.length);
+
+        password += chars.substring(randomNum , randomNum + 1);
+    }
+    input1.value = password;
+}
+
+function copyPassword(){
+    input1.select();
+    input1.setSelectionRange(0,9999);
+}
